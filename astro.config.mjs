c89503eac8f0864d82b79cb1c80bcc9fe7df5e39
kchos0 <hello@kchos0.dev>
2026-02-25 23:37:49 +0700

    add sitemap, favicon, robots.txt, fix accessibility and HTML validity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://kchos0.dev',
   output: 'static',
+  integrations: [sitemap()],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+    },
+  },
 });
